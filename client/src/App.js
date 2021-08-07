f254445ef56f72728053aa3f4ac728503ef3664f
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-08-07 15:32:20 -0700

    apollo fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 /** @format */
 
+
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import {
 	ApolloProvider,
 	ApolloClient,
 	InMemoryCache,
 	createHttpLink,
 } from "@apollo/client";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { setContext } from "@apollo/client/link/context";
 
 import SearchBooks from "./pages/SearchBooks";
@@ -18,15 +20,21 @@ const httpLink = createHttpLink({
 	uri: "/graphql",
 });
 
-const client = new ApolloClient({
-	request: (operation) => {
-		const token = localStorage.getItem("id_token");
-		operation.setContext({
-			headers: { authorization: token ? `Bearer ${token}` : "" },
-		});
-	},
-	uri: "/graphql",
+const authLink = setContext((_, { headers }) => {
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
 });
+
+const client = new ApolloClient({
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
+
 function App() {
 	return (
 		<ApolloProvider client={client}>
